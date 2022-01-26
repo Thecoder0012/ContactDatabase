157043cb9d122d995a426bcc09b8710e38c2fe12
Mardin <mardineliassi57@gmail.com>
2022-01-27 00:00:35 +0100

    Trying to get scanner(system.in) to work with the database. Works fine. Just need to make it even more organised.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add a new person to the database");
+        String name = scanner.nextLine();
+        int phone = scanner.nextInt();
+        String email =  scanner.next();
 
         try {
+
             Connection connection = DriverManager.getConnection("jdbc:sqlite:/IdeaProjects/TestDB/testjava.db");
+//          connection.setAutoCommit(true);
             Statement statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS contacts " + " (name TEXT,phone INTEGER, email TEXT)");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +"" + "VALUES('" +  name + "'," + phone + ",'" + email + "')");
+
+            statement.execute("UPDATE contacts SET phone='"+scanner.nextInt()+"' WHERE name ='"+scanner.next()+"'");
+           statement.execute("DELETE FROM contacts WHERE name='"+scanner.next()+"'");
+
+            statement.execute("SELECT * FROM contacts ");
+            ResultSet resultSet = statement.getResultSet();
+            while(resultSet.next()){
+                System.out.println(
+                        resultSet.getString("name") + " "+
+                        resultSet.getInt("phone") +" " +
+                        resultSet.getString("email"));
+
+//                        resultSet.getString(1)+" "+  easier way, gets the index(column) in the database.
+//                        resultSet.getInt(2)+" "+
+//                        resultSet.getString(3));
+            }
+            resultSet.close();
             statement.close(); // always close statement before connection
             connection.close();
 
